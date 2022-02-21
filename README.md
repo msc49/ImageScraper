262cbf59a25c8ade5cf049ca7fa54de0b1e385f9
Muhammad Choudhry <muhammadshahzaibc@gmail.com>
2022-02-21 23:30:39 +0000

    fixed error of Image.open()

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 Image Scraper
+
+system settings for preference of chromsdriver
+
+https://stackoverflow.com/questions/60362018/macos-catalinav-10-15-3-error-chromedriver-cannot-be-opened-because-the-de
--- main.py
+++ main.py
@@ -9,20 +9,28 @@ class ImageScraper():
   def __init__(self, image_url):
     self.image_url = image_url #url of images
 
-    self.path = "/Users/Shahzaib/Desktop/images/chromedriver.exe" # where we will store image
+    self.path = "/Users/Shahzaib/Desktop/images/chromedriver" # where we will store image
 
     wd = webdriver.Chrome(self.path)
 
 
 
-  def download_image(self,path, url, image_name):
-    image_url_content = requests.get(self.url).content  #contents for the image url
+  def download_image(self, image_name):
+    wd = webdriver.Chrome(self.path)
+    image_url_content = requests.get(self.image_url).content  #contents for the image url
 
     image_file = io.BytesIO(image_url_content) # convert image content to memory
 
-    image = image.open(image_file)
+    image = Image.open(image_file)
 
     file_path = path + image_name
 
     with open(file_path, "wb") as file:
       image.save(file, "JPEG")
+
+    print("successful")
+
+  
+
+t= ImageScraper("https://en.wikipedia.org/wiki/Van_cat#/media/File:VAN_CAT.png")
+t.download_image('cat_image')
