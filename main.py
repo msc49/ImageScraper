80b1b94553e2b75daf6bfa06417b7e8aee76f242
Muhammad Choudhry <muhammadshahzaibc@gmail.com>
2022-02-21 06:42:52 +0000

    fixed class error

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
 from bs4 import BeautifulSoup
 import requests
+
+class ImageScraper():
+
+  def __init__(self, image_url):
+    self.image_url = image_url #url of images
+
+    self.path = "/Users/Shahzaib/Desktop/images" # where we will store image
+
+
