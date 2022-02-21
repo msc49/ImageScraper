316d240ffaf45ea0442064dfc69e05b08ccea2a1
Muhammad Choudhry <muhammadshahzaibc@gmail.com>
2022-02-21 23:05:45 +0000

    added webdriver and path

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 from bs4 import BeautifulSoup
 import requests
+import io
+from selenium import webdriver
 
 class ImageScraper():
 
   def __init__(self, image_url):
     self.image_url = image_url #url of images
 
-    self.path = "/Users/Shahzaib/Desktop/images" # where we will store image
+    self.path = "/Users/Shahzaib/Desktop/images/chromedriver.exe" # where we will store image
+
+    wd = webdriver.Chrome(self.path)
+
+
+
+  def download_image(self,path, url, image_name):
+    image = requests.get(self.url).content
 
 
