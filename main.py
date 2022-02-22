89e48a46c3a6fd816f78727c0bc3e2bb8df22e2d
Muhammad Choudhry <muhammadshahzaibc@gmail.com>
2022-02-22 09:53:09 +0000

    finished get google image function

--- main.py
+++ main.py
@@ -24,11 +24,11 @@ class ImageScraper():
       wd = webdriver.Chrome(self.path)
       image_url_content = requests.get(self.image_url).content  #contents for the image url CHANGEEEE
       image_file = io.BytesIO(image_url_content) # convert image content to memory as binary data
-      image = Image.open(image_file) #converts binary data to PIL image
+      image_pil = Image.open(image_file) #converts binary data to PIL image
       file_path = self.path + image_name
 
       with open(file_path, "wb") as file:
-        image.save(file, "JPEG")
+        image_pil.save(file, "JPEG")
 
     except:
       print('FAILURE- DOWNLOAD IMAGE METHOD DOES DOES NOT WORK')
@@ -53,6 +53,36 @@ class ImageScraper():
     while len(images_urls) < maximum_images:
       self.scroll(wd)
 
+      image_thumbnails = wd.find_element(By.CLASS_NAME, "Q4LuWd") #if you use inspect, you'll see each google image has that class name
+
+      for img in image_thumbnails[len(images_urls): maximum_images]:
+        try:
+          img.click()
+          img.sleep(1)
+
+        except:
+          continue
+
+
+        images = wd.find_elements(By.CLASS_NAME, "n3VNCb")
+        for image in images:
+          if image.get_attribute('src') in images_urls:
+            max_images += 1
+            skips += 1
+            break
+
+
+          if image.get_attribute('src') and 'http' in image.get_attribute('src'):
+            images_urls.add(image.get_attribute('src'))
+            print(f"Found {len(image_urls)}")
+
+
+    return images_urls
+
+    for i, url in enumerate(images_urls):
+      download_image("imgs/", url, str(i) + ".jpg")
+
+    wd.quit()
 
 
 
@@ -60,6 +90,3 @@ class ImageScraper():
 
 
   
-
-t= ImageScraper("https://icatcare.org/app/uploads/2018/07/Thinking-of-getting-a-cat.png")
-t.download_image('cat')
